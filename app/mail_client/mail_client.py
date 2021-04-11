dc11ffd9cd45fe40854bc0c78e57de97dcd43eab
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2021-04-11 20:14:11 +0200

    progress

--- app/mail_client/mail_client.py
+++ app/mail_client/mail_client.py
@@ -1,22 +1,19 @@
 import smtplib
 import ssl
-import sys
+import os
 
 
-class client:
+class Client:
     def __init__(self):
         smtp_adress = 'smtp.gmail.com'
         smtp_port = 465
-        email_adress = 'geoffroypointblondel@gmail.com'
-        email_password = ''
+        username = os.getenv('EMAIL_USERNAME')
+        password = os.getenv('EMAIL_PASSWORD')
+        context = ssl.create_default_context()
 
-# on rentre les informations sur le destinataire
-email_receiver = 'geoffroy.blondel@gmail.com'
-
-# on crée la connexion
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_adress, smtp_port, context=context) as server:
-  # connexion au compte
-  server.login(email_adress, email_password)
-  # envoi du mail
-  server.sendmail(email_adress, email_receiver, 'le contenu de l\'e-mail')
+    def send(self, recipient, subject, message):
+        message = f"Subject: {subject}\n\n{message}"
+        with smtplib.SMTP_SSL(self.smtp_adress, self.smtp_port, context=self.context) as server:
+            server.login(self.username, self.password)
+            server.sendmail(self.username, recipient, message)
+            server.quit()
---
+++ app/mail_client/test_mail_client.py
@@ -0,0 +1,11 @@
+from mail_client import Client
+import unittest
+
+
+class TestClient(unittest.TestCase):
+    def test_send(self):
+        # Given
+        
+        pass
+        # When
+        # Then
